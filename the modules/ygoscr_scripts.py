21f66a9a7806ed6c09d71efd01c638ed73c5a790
ManolisPy <141972176+ManolisPy@users.noreply.github.com>
2025-08-08 14:00:12 +0300

    Add files via upload

---
+++ the modules/ygoscr_scripts.py
@@ -0,0 +1,41 @@
+#-------------------------------------------------------------------------------
+# ygoscr_scripts
+
+# The scripts are created here. The module makes use of ready code
+# from another module.
+#-------------------------------------------------------------------------------
+
+import re
+
+from PyQt6.QtWidgets import QFileDialog, QMessageBox
+from PyQt6.QtCore import QFileInfo
+
+import ygoscr_core_widgets
+import ygoscr_example_scripts
+
+
+
+def file_creation(self):
+
+    if self.corecombo.currentIndex() == 0:
+        QMessageBox.information(self, 'Error','Please select an option.')
+    else:
+        comboboxval = str(self.corecombo.currentIndex())
+        tempname, _ = QFileDialog.getSaveFileName(self,
+                                                     'Save as', "scriptno" + comboboxval, 'Lua Files (*.lua)')
+        if not tempname:
+            return
+        else:
+            savedFile = QFileInfo(tempname)
+
+        if savedFile:
+            codenum = savedFile.baseName()
+
+        file = open(savedFile.absoluteFilePath(), 'w')
+        if self.corecombo.currentIndex() == 1:
+            file.write(ygoscr_example_scripts.spell_card)
+        elif self.corecombo.currentIndex() == 2:
+            file.write(ygoscr_example_scripts.trap_card)
+
+        file.close()
+        QMessageBox.information(self, 'Success','Script creation completed!')
